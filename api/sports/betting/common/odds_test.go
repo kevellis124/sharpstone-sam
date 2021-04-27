b8209ddf3af2237f7983ff2e06edc7f8d1ab5440
Kevin Ellis <kevin.ellis@excella.com>
2021-04-27 11:21:19 -0700

    Add stubbed method to retrieve todays odds for a sport. Add a test.

---
+++ api/sports/betting/common/odds_test.go
@@ -0,0 +1,18 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+	t.Run("Returns a stubbed list of nba odds", func(t *testing.T) {
+		result, err := getTodaysOdds("nba")
+
+		if err != nil {
+			t.Fatal("Function returned an error when it shouldn't")
+		}
+		assert.Equal(t, "test", result)
+	})
+}
